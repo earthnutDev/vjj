1ecb0b0f0c99c600287c4fe87f084bd15f535bca
earth nut dev <earthnut.dev@outlook.com>
2025-05-03 20:38:19 +0800

    更新依赖和更改打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -2,49 +2,9 @@ import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
-// import terser from '@rollup/plugin-terser';
 import cleanup from 'rollup-plugin-cleanup';
 import copy from 'rollup-plugin-copy';
-
-import { readFileToJsonSync } from 'a-node-tools';
-import { pen } from 'color-pen';
-
-/** 配置需要不打包进生产包的包名配置  */
-const excludedPkg = ['node:', 'a-', 'color-pen', '@qqi'];
-
-const excludedRegExp = new RegExp('^'.concat(excludedPkg.join('|^')));
-/**  读取当前文件  */
-const packInfo = readFileToJsonSync('./package.json');
-/**  已配置的依赖  */
-const dependencies = Object.keys({
-  ...(packInfo.dependencies || {}),
-  ...(packInfo.preDependencies || {}),
-});
-
-/**
- *
- * 依赖配置
- *
- */
-function external(id) {
-  excludedRegExp.lastIndex = 0;
-  const result = excludedRegExp.test(id);
-  /// 保证排除的包纯在于
-  if (result === true) {
-    if (dependencies.includes(id) === false && !id.startsWith('node:')) {
-      throw new RangeError(
-        `${pen.bgRed.blink.bold.yellow(id)} 依赖被排除打包却未再 package.json 中配置`,
-      );
-    }
-  } else {
-    if (/^[^./]/g.test(id)) {
-      throw new RangeError(
-        `${pen.bgRed.blink.bold.yellow(id)} 依赖未被排除，打包关闭`,
-      );
-    }
-  }
-  return result;
-}
+import { external } from '@qqi/rollup-external';
 export default {
   input: './bin.ts',
   output: {
@@ -56,19 +16,15 @@ export default {
     dir: 'dist/',
   },
   // 配置需要排除的包
-  external,
+  external: external({
+    ignore: ['node:fs'],
+  }),
   plugins: [
     resolve(),
     commonjs(),
     // 可打包 json 内容
     json(),
-    typescript({
-      // compilerOptions: {},
-      // exclude: ["./node_modules", "./test"],
-    }),
-    // 打包压缩，自动去注释
-    // terser(),
-    // 去除无用代码
+    typescript({}),
     cleanup(),
     copy({
       targets: [
--- scripts/pub.sh
+++ scripts/pub.sh
@@ -2,16 +2,34 @@
 
 npm ci
 
-npm run build
+
+if ! npm run build; then 
+  echo "构建失败" 
+  exit 1
+fi
 
 VERSION=$(node -p "require('./package.json').version")
+
 echo "获取全称 npm version : $VERSION"
 if [[ $VERSION =~ -([a-zA-Z0-9]+)(\.|$) ]]; then
   TAG=${BASH_REMATCH[1]}
   echo "捕获到 npm tag : $TAG"
 else
   TAG="latest"
-  echo "未捕获到 npm tag 设置了默认 : $TAG"
+  echo "未捕获到 npm tag 使用默认 : $TAG"
 fi
-cd dist
-npm publish --provenance --access public --tag ${TAG}
+
+if ! cd dist; then 
+  echo "未找到 dist 构建码"
+  exit 1
+fi
+
+echo "开始发布 npm 包"
+
+if ! npm publish --provenance --access public --tag ${TAG} ; then
+    echo "发布失败" 
+    exit 1
+fi
+
+echo "🚀🚀  发布成功，完结 🎉🎉 撒花 🎉🎉"
+
