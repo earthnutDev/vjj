22781e36d0eb3b3fafcb863a9f1100dd88f5a1db
earth nut dev <earthnut.dev@outlook.com>
2025-07-16 01:03:06 +0800

    version : v1.0.8 2025/7/16 01:03:06

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,14 @@
-import { cursorAfterClear } from 'a-node-tools';
+import { _p, cursorAfterClear } from 'a-node-tools';
 import command from './command';
 import { waiting } from './waiting';
 import { getRandomInt, sleep } from 'a-js-tools';
+import { cyanPen, greenPen } from 'color-pen';
+import { dataStore } from './data-store';
 
 /**  退出程序  */
 export async function exitProgram(info: string = '您选择了退出，正在为您退出') {
+  const { version } = dataStore.originalVersion;
+  const { onlineData } = dataStore;
   waiting.run({
     info,
     prefix: getRandomInt(5),
@@ -12,5 +16,11 @@ export async function exitProgram(info: string = '您选择了退出，正在为
   await sleep(890);
   waiting.destroyed();
   cursorAfterClear();
+  if (version) {
+    _p(`当前应用的版本是 ${cyanPen(version)}`);
+  }
+  if (onlineData.info?.version) {
+    _p(`当前线上版本为 ${greenPen(onlineData.info?.version)}`);
+  }
   return command.end();
 }
